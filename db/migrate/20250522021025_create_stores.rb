c265f46425aab93f776e5ec3b0a47a22d783520b
Lauren Friedmann <Lauren.friedmann@sameskyhealth.com>
2025-05-24 15:36:25 -0400

    Initial Commit and Migrations

---
+++ db/migrate/20250522021025_create_stores.rb
@@ -0,0 +1,11 @@
+class CreateStores < ActiveRecord::Migration[7.2]
+  def change
+    create_table :stores do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
